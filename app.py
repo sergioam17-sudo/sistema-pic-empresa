6824cbd10030e403b95ff0e6390abd2ba4065b4b
sergioam17-sudo <sergioam17@gmail.com>
2026-05-12 12:17:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def connection():
     # USAMOS EL HOST DIRECTO PARA EVITAR EL TIMEOUT DEL POOLER
     USER = "postgres" 
     PASS = "ClavePic2026" # <--- Si cambiaste la clave en Supabase, ponla aquí
-    HOST = "ewsfasbgcewaarmsfqbt.supabase.co" 
+    HOST = "db.ewsfasbgcewaarmsfqbt.supabase.co" 
     PORT = "5432"
     DBNAME = "postgres"
     
